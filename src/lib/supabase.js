0a66ee49b00d01f6777e3b9ea2f89891f2868703
RijalR <RijalR>
2026-03-25 15:15:02 +0700

    chore: bersih-bersih file yang tidak digunakan dan reset git

---
+++ src/lib/supabase.js
@@ -0,0 +1,11 @@
+// src/lib/supabase.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const SECRET_KEY = import.meta.env.PUBLIC_SECRET_KEY;
+
+export const URL_CEK_NIK = import.meta.env.PUBLIC_URL_CEK_NIK;
